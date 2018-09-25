e7fd0e8a22036a9023485e8eb071c1bc63f989cf
Jakub Draganek <jakub.draganek@gmail.com>
2018-09-25 10:59:34 +0200

    Move test to modularization suite

--- tests/test_modularization.py
+++ tests/test_modularization.py
@@ -1,6 +1,8 @@
+from unittest.mock import Mock
+
 from graphql import graphql
 
-from ariadne import make_executable_schema
+from ariadne import make_executable_schema, resolve_to
 
 root_typedef = """
     type Query {
@@ -51,3 +53,42 @@ def test_override_typedef_outdated_field():
     assert result.errors is not None
     assert str(result.errors[0]) == 'Cannot query field "username" on type "User".'
     assert result.data is None
+
+
+def test_accept_list_of_resolvers_maps():
+    type_defs = """
+        type Query {
+            user: User
+            test(data: TestInput): Int
+        }
+
+        type User {
+            firstName: String
+        }
+
+        input TestInput {
+            value: Int
+        }
+    """
+
+    def resolve_test(*_, data):
+        assert data == {"value": 4}
+        return "42"
+
+    resolvers = [
+        {
+            "Query": {"user": lambda *_: Mock(first_name="Joe")},
+            "User": {"firstName": resolve_to("first_name")},
+        },
+        {"Query": {"test": resolve_test}},
+    ]
+
+    schema = make_executable_schema(type_defs, resolvers)
+
+    result = graphql(schema, "{ test(data: { value: 4 }) }")
+    assert result.errors is None
+    assert result.data == {"test": 42}
+
+    result = graphql(schema, "{ user { firstName } }")
+    assert result.errors is None
+    assert result.data == {"user": {"firstName": "Joe"}}

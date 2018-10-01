5a68b11a00923fa5b75f81bb38fec902355a1e11
Rafał Pitoń <rafal@mirumee.com>
2018-10-01 11:51:23 +0200

    Order imports

--- ariadne/__init__.py
+++ ariadne/__init__.py
@@ -1,7 +1,7 @@
 from .build_schema import build_schema_from_type_definitions
 from .executable_schema import make_executable_schema
-from .wsgi import GraphQLMiddleware
 from .resolvers import add_resolve_functions_to_schema, default_resolver, resolve_to
+from .wsgi import GraphQLMiddleware
 
 __all__ = [
     "GraphQLMiddleware",

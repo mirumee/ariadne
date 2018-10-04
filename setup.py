eb3f27e7f29610ea3cea433958986fdfbafe1ca7
Rafał Pitoń <rafal@mirumee.com>
2018-10-04 16:29:08 +0200

    Autoformat and pylint setup.py

--- setup.py
+++ setup.py
@@ -2,24 +2,26 @@
 from setuptools import setup
 
 CLASSIFIERS = [
-    'Intended Audience :: Developers',
-    'License :: OSI Approved :: BSD License',
-    'Operating System :: OS Independent',
-    'Programming Language :: Python',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-    'Programming Language :: Python :: 3.7',
-    'Topic :: Software Development :: Libraries :: Python Modules']
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: BSD License",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+]
 
 setup(
-    name='ariadne',
-    author='Mirumee Software',
-    author_email='hello@mirumee.com',
-    description='A functional implementation of a Python GraphQL server',
-    license='BSD',
-    version='0.0.2',
-    url='https://github.com/mirumee/ariadne',
-    packages=['ariadne'],
-    install_requires=['graphql-core>=2.1', 'typing>=3.6.0'],
+    name="ariadne",
+    author="Mirumee Software",
+    author_email="hello@mirumee.com",
+    description="A functional implementation of a Python GraphQL server",
+    license="BSD",
+    version="0.0.2",
+    url="https://github.com/mirumee/ariadne",
+    packages=["ariadne"],
+    install_requires=["graphql-core>=2.1", "typing>=3.6.0"],
     classifiers=CLASSIFIERS,
-    platforms=['any'])
+    platforms=["any"],
+)

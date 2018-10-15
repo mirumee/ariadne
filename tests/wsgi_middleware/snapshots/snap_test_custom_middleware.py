dfd5e51189de14006e1a37fbb3e0586988363f7d
Rafał Pitoń <rafal@mirumee.com>
2018-10-15 14:16:11 +0200

    Commit tests using new assertion

--- tests/wsgi_middleware/snapshots/snap_test_custom_middleware.py
+++ tests/wsgi_middleware/snapshots/snap_test_custom_middleware.py
@@ -7,14 +7,10 @@ from snapshottest import Snapshot
 
 snapshots = Snapshot()
 
-snapshots['test_custom_middleware_executes_query_with_custom_query_context 1'] = {
-    'data': {
-        'hasValidAuth': True
-    }
+snapshots["test_custom_middleware_executes_query_with_custom_query_context 1"] = {
+    "data": {"hasValidAuth": True}
 }
 
-snapshots['test_custom_middleware_executes_query_with_custom_query_root 1'] = {
-    'data': {
-        'user': True
-    }
+snapshots["test_custom_middleware_executes_query_with_custom_query_root 1"] = {
+    "data": {"user": True}
 }
--- tests/wsgi_middleware/snapshots/snap_test_query_execution.py
+++ tests/wsgi_middleware/snapshots/snap_test_query_execution.py
@@ -7,66 +7,39 @@ from snapshottest import Snapshot
 
 snapshots = Snapshot()
 
-snapshots['test_query_is_executed_for_post_json_request 1'] = {
-    'data': {
-        'status': True
-    }
+snapshots["test_query_is_executed_for_post_json_request 1"] = {"data": {"status": True}}
+
+snapshots["test_complex_query_is_executed_for_post_json_request 1"] = {
+    "data": {"hello": "Hello, Bob!"}
 }
 
-snapshots['test_complex_query_is_executed_for_post_json_request 1'] = {
-    'data': {
-        'hello': 'Hello, Bob!'
-    }
+snapshots["test_complex_query_without_operation_name_executes_successfully 1"] = {
+    "data": {"hello": "Hello, Bob!"}
 }
 
-snapshots['test_complex_query_without_operation_name_executes_successfully 1'] = {
-    'data': {
-        'hello': 'Hello, Bob!'
-    }
-}
-
-snapshots['test_attempt_execute_complex_query_without_variables_returns_error_json 1'] = {
-    'errors': [
+snapshots[
+    "test_attempt_execute_complex_query_without_variables_returns_error_json 1"
+] = {
+    "errors": [
         {
-            'locations': [
-                {
-                    'column': 18,
-                    'line': 2
-                }
-            ],
-            'message': 'Variable "$name" of required type "String!" was not provided.'
+            "locations": [{"column": 18, "line": 2}],
+            "message": 'Variable "$name" of required type "String!" was not provided.',
         }
     ]
 }
 
-snapshots['test_attempt_execute_query_without_query_entry_returns_error_json 1'] = {
-    'errors': [
-        {
-            'message': 'The query must be a string'
-        }
-    ]
+snapshots["test_attempt_execute_query_without_query_entry_returns_error_json 1"] = {
+    "errors": [{"message": "The query must be a string"}]
 }
 
-snapshots['test_attempt_execute_query_with_non_string_query_returns_error_json 1'] = {
-    'errors': [
-        {
-            'message': 'The query must be a string'
-        }
-    ]
+snapshots["test_attempt_execute_query_with_non_string_query_returns_error_json 1"] = {
+    "errors": [{"message": "The query must be a string"}]
 }
 
-snapshots['test_attempt_execute_query_with_invalid_variables_returns_error_json 1'] = {
-    'errors': [
-        {
-            'message': 'Query variables must be a null or an object'
-        }
-    ]
+snapshots["test_attempt_execute_query_with_invalid_variables_returns_error_json 1"] = {
+    "errors": [{"message": "Query variables must be a null or an object"}]
 }
 
-snapshots['test_attempt_execute_query_with_invalid_operation_name_returns_error_json 1'] = {
-    'errors': [
-        {
-            'message': 'Unknown operation named "[1, 2, 3]".'
-        }
-    ]
-}
+snapshots[
+    "test_attempt_execute_query_with_invalid_operation_name_returns_error_json 1"
+] = {"errors": [{"message": 'Unknown operation named "[1, 2, 3]".'}]}

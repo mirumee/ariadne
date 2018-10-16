c67df59f11559ca963f8acbdaf9b41cc520edc6d
Jakub Draganek <jakub.draganek@gmail.com>
2018-10-16 16:36:39 +0200

    Test basic enum definition

---
+++ tests/test_enums.py
@@ -0,0 +1,37 @@
+from ariadne import make_executable_schema
+from graphql import graphql
+
+type_defs = """
+    enum Episode {
+        NEWHOPE
+        EMPIRE
+        JEDI
+    }
+
+    type Query {
+        testEnum: Episode!
+    }
+"""
+
+
+def resolve_test_enum(*_):
+    return "NEWHOPE"
+
+
+
+def test_succesfull_enum():
+    resolvers = {
+        "Query": {"testEnum": resolve_test_enum},
+    }
+    schema = make_executable_schema(type_defs, resolvers)
+    result = graphql(schema, "{ testEnum }")
+    assert result.errors is None
+
+
+def test_failed_enum():
+    resolvers = {
+        "Query": {"testEnum": lambda *_: "LUKE"},
+    }
+    schema = make_executable_schema(type_defs, resolvers)
+    result = graphql(schema, "{ testEnum }")
+    assert result.errors is not None

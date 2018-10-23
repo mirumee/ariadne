eaf05db073b5cda379636bcbb4c5a8297072bc6a
rafalp <kontakt@rpiton.com>
2018-10-23 19:11:00 +0200

    Bump version to 0.0.4

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     author_email="hello@mirumee.com",
     description="A functional implementation of a Python GraphQL server",
     license="BSD",
-    version="0.0.3",
+    version="0.0.4",
     url="https://github.com/mirumee/ariadne",
     packages=["ariadne"],
     install_requires=["graphql-core>=2.1", "typing>=3.6.0"],

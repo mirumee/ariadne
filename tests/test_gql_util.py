427e61ece1ab551b9403e1fb940d402423269ac8
Rafał Pitoń <rafal@mirumee.com>
2018-11-21 11:44:15 +0100

    Tweak tests a little

--- tests/test_gql_util.py
+++ tests/test_gql_util.py
@@ -4,7 +4,7 @@ from graphql.error import GraphQLSyntaxError
 from ariadne import gql
 
 
-def test_valid_graphqll_string_is_passed_as_is():
+def test_valid_graphql_schema_string_is_returned_unchanged():
     sdl = """
         type User {
             username: String!
@@ -14,12 +14,41 @@ def test_valid_graphqll_string_is_passed_as_is():
     assert sdl == result
 
 
-def test_invalid_graphql_string_raises_syntax_error():
+def test_invalid_graphql_schema_string_causes_syntax_error():
     with pytest.raises(GraphQLSyntaxError):
         gql(
             """
                 type User {
                     username String!
+                }
+            """
+        )
+
+
+def test_valid_graphql_query_string_is_returned_unchanged():
+    query = """
+        query TestQuery {
+            auth
+            users {
+                id
+                username
+            }
+        }
+    """
+    result = gql(query)
+    assert query == result
+
+
+def test_invalid_graphql_query_string_causes_syntax_error():
+    with pytest.raises(GraphQLSyntaxError):
+        gql(
+            """
+                query TestQuery {
+                    auth
+                    users 
+                        id
+                        username
+                    }
                 }
             """
         )

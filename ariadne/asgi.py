15cbaf69ffe5f37ccac2c193bb271e1464c5e478
Artur Smęt <artur@mirumee.com>
2019-06-03 13:22:51 +0200

    WIP: Add file upload support using multipart/form-data

--- ariadne/asgi.py
+++ ariadne/asgi.py
@@ -1,13 +1,15 @@
 import asyncio
+import json
 from typing import Any, AsyncGenerator, Dict, List, Optional, Tuple, cast
 
 from graphql import GraphQLError, GraphQLSchema
+from starlette.datastructures import UploadFile
 from starlette.requests import Request
 from starlette.responses import HTMLResponse, JSONResponse, PlainTextResponse, Response
 from starlette.types import Receive, Scope, Send
 from starlette.websockets import WebSocket, WebSocketState, WebSocketDisconnect
 
-from .constants import DATA_TYPE_JSON, PLAYGROUND_HTML
+from .constants import DATA_TYPE_JSON, PLAYGROUND_HTML, DATA_TYPE_MULTIPART
 from .exceptions import HttpBadRequestError, HttpError
 from .format_error import format_error
 from .graphql import graphql, subscribe
@@ -27,6 +29,71 @@ GQL_DATA = "data"  # Server -> Client
 GQL_ERROR = "error"  # Server -> Client
 GQL_COMPLETE = "complete"  # Server -> Client
 GQL_STOP = "stop"  # Client -> Server
+
+#### TMP
+
+def place_files_in_operations(operations, files_map, files):
+    """Replaces None placeholders in operations with file objects in the files
+    dictionary, by following the files_map logic as specified within the 'map'
+    request parameter in the multipart request spec"""
+    path_to_key_iter = (
+        (value.split('.'), key)
+        for (key, values) in files_map.items()
+        for value in values
+    )
+    # Since add_files_to_operations returns a new dict/list, first define
+    # output to be operations itself
+    output = operations
+    for path, key in path_to_key_iter:
+        file_obj = files[key]
+        output = add_file_to_operations(output, file_obj, path)
+    return output
+
+
+def add_file_to_operations(operations, file_obj, path):
+    """Handles the recursive algorithm for adding a file to the operations
+    object"""
+    if not path:
+        if operations is not None:
+            raise ValueError('Path in map does not lead to a null value')
+        return file_obj
+    if isinstance(operations, dict):
+        key = path[0]
+        sub_dict = add_file_to_operations(operations[key], file_obj, path[1:])
+        return new_merged_dict(operations, {key: sub_dict})
+    if isinstance(operations, list):
+        index = int(path[0])
+        sub_item = add_file_to_operations(
+            operations[index],
+            file_obj,
+            path[1:],
+        )
+        return new_list_with_replaced_item(operations, index, sub_item)
+    raise TypeError('Operations must be a dict or a list of dicts')
+
+
+def new_merged_dict(*dicts):
+    """Merges dictionaries into a new dictionary. Necessary for python2 and
+    python34 since neither have PEP448 implemented."""
+    # Necessary for python2 support
+    output = {}
+    for d in dicts:
+        output.update(d)
+    return output
+
+
+def new_list_with_replaced_item(input_list, index, new_value):
+    """Creates new list with replaced item at specified index"""
+    output = [i for i in input_list]
+    output[index] = new_value
+    return output
+
+
+####
+
+
+
+
 
 
 class GraphQL:
@@ -79,15 +146,36 @@ class GraphQL:
     async def extract_data_from_request(
         self, request: Request
     ) -> Tuple[str, Optional[dict], Optional[str]]:
-        if request.headers.get("Content-Type") != DATA_TYPE_JSON:
-            raise HttpBadRequestError(
-                "Posted content must be of type {}".format(DATA_TYPE_JSON)
-            )
-        try:
-            return await request.json()
-        except ValueError:
-            raise HttpBadRequestError("Request body is not a valid JSON")
 
+        content_type = request.headers.get("Content-Type")
+        content_type = content_type.split(";")[0]
+
+        if content_type == DATA_TYPE_JSON:
+            try:
+                return await request.json()
+            except ValueError:
+                raise HttpBadRequestError("Request body is not a valid JSON")
+        if content_type == DATA_TYPE_MULTIPART:
+            try:
+                request_body = await request.form()
+            except ValueError:
+                raise HttpBadRequestError("Request body is not a valid multipart/form-data")
+
+            operations = json.loads(request_body.get("operations", {}))
+            files_map = json.loads(request_body.get('map', {}))
+            request_files = {
+                key: value for key, value in request_body.items()
+                if isinstance(value, UploadFile)}
+            result = place_files_in_operations(
+                operations,
+                files_map,
+                request_files
+            )
+            return result
+
+        raise HttpBadRequestError(
+                "Posted content must be of type {} or {}".format(DATA_TYPE_JSON, DATA_TYPE_MULTIPART)
+            )
     async def render_playground(  # pylint: disable=unused-argument
         self, request: Request
     ) -> Response:

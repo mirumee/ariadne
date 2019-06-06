acbac377f1ed659a71bb420f6fc72049fde0270f
Rafał Pitoń <rafal@mirumee.com>
2019-06-06 17:42:28 +0200

    Bump version to 0.5.0

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     long_description=README,
     long_description_content_type="text/markdown",
     license="BSD",
-    version="0.5.0b1",
+    version="0.5.0",
     url="https://github.com/mirumee/ariadne",
     packages=["ariadne"],
     include_package_data=True,

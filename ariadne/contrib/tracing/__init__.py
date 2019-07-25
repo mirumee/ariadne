d6856f1760158834c542c1a9c12f47afb3906957
Rafał Pitoń <rafal@mirumee.com>
2019-07-25 14:38:30 +0200

    Test tracing utils

--- ariadne/contrib/tracing/__init__.py
+++ ariadne/contrib/tracing/__init__.py
@@ -1,3 +1,3 @@
 from .apollo import ApolloTracingExtension
 from .opentracing import OpenTracingExtension, opentracing_extension
-from .utils import format_path, should_trace
+from .utils import format_path, is_introspection_field, should_trace
--- ariadne/contrib/tracing/utils.py
+++ ariadne/contrib/tracing/utils.py
@@ -1,5 +1,7 @@
 from graphql import GraphQLResolveInfo, ResponsePath
 
+from ...resolvers import is_default_resolver
+
 
 def format_path(path: ResponsePath):
     elements = []
@@ -10,9 +12,20 @@ def format_path(path: ResponsePath):
 
 
 def should_trace(info: GraphQLResolveInfo):
+    resolver = info.parent_type.fields[info.field_name].resolve
+    if (
+        resolver is None
+        or is_default_resolver(resolver)
+        or is_introspection_field(info)
+    ):
+        return False
+    return True
+
+
+def is_introspection_field(info: GraphQLResolveInfo):
     path = info.path
     while path:
         if isinstance(path.key, str) and path.key.startswith("__"):
-            return False
+            return True
         path = path.prev
-    return True
+    return False

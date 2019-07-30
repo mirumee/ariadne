fe9dac3163c1bc7f6d8e97f6e83a9735e23a7a12
Yasin Bahtiyar <yasin@bahtiyar.org>
2019-07-30 09:57:09 +0200

    Split single and multiple directives tests

--- tests/test_directives.py
+++ tests/test_directives.py
@@ -11,7 +11,39 @@ from ariadne import (
 )
 
 
-def test_directives():
+class UpperDirective(SchemaDirectiveVisitor):
+    def visit_field_definition(
+        self,
+        field: GraphQLField,
+        object_type: Union[GraphQLObjectType, GraphQLInterfaceType],
+    ) -> GraphQLField:
+        original_resolver = field.resolve or default_field_resolver
+
+        def resolve_upper(obj, info, **kwargs):
+            result = original_resolver(obj, info, **kwargs)
+            return result.upper()
+
+        field.resolve = resolve_upper
+        return field
+
+
+class ReverseDirective(SchemaDirectiveVisitor):
+    def visit_field_definition(
+        self,
+        field: GraphQLField,
+        object_type: Union[GraphQLObjectType, GraphQLInterfaceType],
+    ) -> GraphQLField:
+        original_resolver = field.resolve or default_field_resolver
+
+        def resolve_reverse(obj, info, **kwargs):
+            result = original_resolver(obj, info, **kwargs)
+            return result[::-1]
+
+        field.resolve = resolve_reverse
+        return field
+
+
+def test_single_directive_without_args():
     type_defs = """
         directive @upper on FIELD_DEFINITION
         directive @reverse on FIELD_DEFINITION
@@ -22,39 +54,9 @@ def test_directives():
 
         type Custom {
             node: String @upper
-            name: String @reverse @upper
+            name: String @reverse
         }
     """
-
-    class UpperDirective(SchemaDirectiveVisitor):
-        def visit_field_definition(
-            self,
-            field: GraphQLField,
-            object_type: Union[GraphQLObjectType, GraphQLInterfaceType],
-        ) -> GraphQLField:
-            original_resolver = field.resolve or default_field_resolver
-
-            def resolve_upper(obj, info, **kwargs):
-                result = original_resolver(obj, info, **kwargs)
-                return result.upper()
-
-            field.resolve = resolve_upper
-            return field
-
-    class ReverseDirective(SchemaDirectiveVisitor):
-        def visit_field_definition(
-            self,
-            field: GraphQLField,
-            object_type: Union[GraphQLObjectType, GraphQLInterfaceType],
-        ) -> GraphQLField:
-            original_resolver = field.resolve or default_field_resolver
-
-            def resolve_upper(obj, info, **kwargs):
-                result = original_resolver(obj, info, **kwargs)
-                return result[::-1]
-
-            field.resolve = resolve_upper
-            return field
 
     query = QueryType()
     query.set_field("test", lambda *_: {"node": "custom", "name": "uppercase"})
@@ -65,4 +67,26 @@ def test_directives():
 
     result = graphql_sync(schema, "{ test { node name }}")
     assert result.errors is None
-    assert result.data == {"test": {"node": "CUSTOM", "name": "ESACREPPU"}}
+    assert result.data == {"test": {"node": "CUSTOM", "name": "esacreppu"}}
+
+
+def test_many_directives_without_args():
+    type_defs = """
+        directive @upper on FIELD_DEFINITION
+        directive @reverse on FIELD_DEFINITION
+
+        type Query {
+          hello: String @upper @reverse
+        }
+    """
+
+    query = QueryType()
+    query.set_field("hello", lambda *_: "hello world")
+    upper_dir = DirectiveType("upper", UpperDirective)
+    reverse_dir = DirectiveType("reverse", ReverseDirective)
+
+    schema = make_executable_schema(type_defs, [query, upper_dir, reverse_dir])
+
+    result = graphql_sync(schema, "{ hello }")
+    assert result.errors is None
+    assert result.data == {"hello": "DLROW OLLEH"}

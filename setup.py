428f3577a2d3f5da7f1da597dee5688a22527535
rafalp <kontakt@rpiton.com>
2020-03-24 21:56:23 +0100

    Bump starlette to 0.13.2

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     long_description=README,
     long_description_content_type="text/markdown",
     license="BSD",
-    version="0.10.0",
+    version="0.11.0",
     url="https://github.com/mirumee/ariadne",
     packages=["ariadne"],
     include_package_data=True,

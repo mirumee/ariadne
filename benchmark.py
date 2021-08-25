524713bd0f270d5663f32fe198beeb412252e4a7
Kuba <kuba.klimas1@gmail.com>
2021-08-25 11:53:54 +0200

    added threads to start server and send requests to it

--- benchmark.py
+++ benchmark.py
@@ -1,9 +1,13 @@
-import asyncio, random, string, statistics
+import random
+import string
+import statistics
+import threading
 from typing import List, Tuple
 
 import httpx
+import uvicorn 
 
-from ariadne import ObjectType, QueryType, gql, make_executable_schema
+from ariadne import QueryType, gql, make_executable_schema
 from ariadne.asgi import GraphQL
 
 
@@ -25,7 +29,7 @@ simple_query = {"query": simple_query_formatter("person")}
 list_simple_query = {"query": simple_query_formatter("people")}
 
 
-def measure_post_time(query, n: int = 100) -> Tuple:
+def measure_post_time(query, n: int = 10) -> Tuple:
     time_list = []
     for i in range(n):
         request = httpx.post("http://127.0.0.1:8000/", json=query)
@@ -40,7 +44,7 @@ def measure_post_time(query, n: int = 100) -> Tuple:
 
 def make_random_name_and_age(n: int) -> List[Tuple]:
     people_list = []
-    for i in range(n):
+    for _ in range(n):
         generate_name = random.sample(string.ascii_letters, 10)
         name = "".join(generate_name)
         age = random.randint(1, 99)
@@ -81,3 +85,23 @@ def resolve_people(*_):
 schema = make_executable_schema(type_defs, query)
 
 app = GraphQL(schema, debug=True)
+
+def post_after_server_start(n: int = 10):
+    result_list = []
+    result_list.append(measure_post_time(simple_query, n))
+    result_list.append(measure_post_time(list_simple_query, n))
+
+    print(result_list)
+    return result_list
+
+
+def start_server():
+    uvicorn.run(app, host="127.0.0.1", port=8000, log_level="info")
+
+
+if __name__ == '__main__':
+    t1 = threading.Thread(target=start_server)
+    t2 = threading.Thread(target=post_after_server_start)
+    
+    t1.start()
+    t2.start()

158f0c8cfd1241f620560bfc4746d8c0c9ded1c8
Kuba <kuba.klimas1@gmail.com>
2021-08-31 15:09:29 +0200

    added complex data type, generate complex json

--- benchmarks/json_data.py
+++ benchmarks/json_data.py
@@ -5,8 +5,7 @@ import string
 from typing import List, Tuple
 
 
-
-def make_random_name_and_age(n: int) -> List[Tuple]:
+def make_random_simple_query(n: int) -> List[Tuple]:
     people_list = []
     for _ in range(n):
         generate_name = random.sample(string.ascii_letters, 10)
@@ -15,7 +14,30 @@ def make_random_name_and_age(n: int) -> List[Tuple]:
         people_list.append({"name": name, "age": age})
     return people_list
 
-f = open('simple.json',"w")
-with open('benchmarks/simple.json','w') as file:
-    file.write(json.dumps(make_random_name_and_age(500),indent=2))
 
+def make_random_complex_query(n: int) -> List[Tuple]:
+    ROLES = ["SEE", "BROWSE", "START", "REPLY", "MODERATE"]
+    complex_query = []
+
+    for i in range(n):
+        id = i
+        name = "".join(random.sample(string.ascii_letters, 10))
+        group = {
+            "name": "".join(random.sample(string.ascii_letters, 3)),
+            "roles": [random.choice(ROLES)],
+        }
+        avatar = [
+            {
+                "size": random.randint(1, 200),
+                "url": "".join(random.sample(string.ascii_letters, 10)),
+            }
+        ]
+        complex_query.append({"id": id, "name": name, "group": group, "avatar": avatar})
+    return complex_query
+
+
+with open("benchmarks/simple.json", "w") as simple_file:
+    simple_file.write(json.dumps(make_random_simple_query(500), indent=2))
+
+with open("benchmarks/complex.json", "w") as complex_file:
+    complex_file.write(json.dumps(make_random_complex_query(500), indent=2))
--- benchmarks/main.py
+++ benchmarks/main.py
@@ -1,5 +1,4 @@
 import json
-from typing import Tuple
 
 from fastapi.testclient import TestClient
 
@@ -7,7 +6,7 @@ from ariadne import QueryType, gql, make_executable_schema
 from ariadne.asgi import GraphQL
 
 
-def benchmark_simple_list(query: str) -> Tuple:
+def benchmark_simple_list(query: str):
     with open("benchmarks/simple.json") as f:
         data = json.load(f)
 
@@ -18,8 +17,35 @@ def benchmark_simple_list(query: str) -> Tuple:
     return request.status_code
 
 
-def benchmark_simple(query: str) -> Tuple:
+def benchmark_simple(query: str):
     data = [{"name": "John", "age": 23}]
+    app = GraphQL(schema, root_value=data)
+    client = TestClient(app)
+    request = client.post("/", json={"query": query})
+
+    return request.status_code
+
+
+def benchmark_complex_list(query: str):
+    with open("benchmarks/complex.json", "r") as file:
+        data = json.load(file)
+
+    app = GraphQL(schema, root_value=data)
+    client = TestClient(app)
+    request = client.post("/", json={"query": query})
+
+    return request.status_code
+
+
+def benchmark_complex(query: str):
+    data = [
+        {
+            "id": 1,
+            "name": "John",
+            "group": {"name": "name1", "roles": ["SEE"]},
+            "avatar": [{"size": 123, "url": "http://127.0.0.1"}],
+        }
+    ]
     app = GraphQL(schema, root_value=data)
     client = TestClient(app)
     request = client.post("/", json={"query": query})
@@ -32,11 +58,40 @@ type_defs = gql(
     type Query {
         people: [Person!]!
         person: [Person!]!
+        users: [User!]
+        user: [User!]
     }
 
     type Person {
         name: String
         age: Int
+    }
+
+
+    type User {
+        id: ID!
+        name: String!
+        title: String
+        group: Group!
+        avatar: [Avatar!]!
+    }
+
+    type Group {
+        name: String!
+        roles: [Role!]!
+    }
+
+    type Avatar {
+        size: Int!
+        url: String!
+    }
+
+    enum Role {
+        SEE
+        BROWSE
+        START
+        REPLY
+        MODERATE
     }
 """
 )
@@ -54,6 +109,26 @@ def resolve_people(*_):
 @query.field("person")
 def resolve_person(*_):
     return [{"name": "John", "age": 23}]
+
+
+@query.field("users")
+def resolve_users(*_):
+    with open("benchmarks/complex.json", "r") as file:
+        data = json.load(file)
+    return data
+
+
+@query.field("user")
+def resolve_user(*_):
+    user = [
+        {
+            "id": 1,
+            "name": "John",
+            "group": {"name": "n1", "roles": ["SEE"]},
+            "avatar": [{"size": 123, "url": "sdkasdoa"}],
+        }
+    ]
+    return user
 
 
 schema = make_executable_schema(type_defs, query)

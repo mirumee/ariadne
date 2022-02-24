a56f3eab4f49b97d47ae7caed25cf9f96f321cd2
Fernando Soares <fss@lionx.com.br>
2022-02-23 23:43:55 -0300

    chore: formatting using black

--- ariadne/contrib/subscriptions/subscription.py
+++ ariadne/contrib/subscriptions/subscription.py
@@ -5,4 +5,3 @@ class Subscription:
     def __init__(self, operation_name: str, async_generator: AsyncGenerator):
         self.operation_name = operation_name
         self.async_generator = async_generator
-

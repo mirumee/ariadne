035ba3d01f98e808aa2bbb97af3d70635ce7768a
Itamar <ilevin@wayfair.com>
2022-11-02 11:19:04 -0400

    fix lints

--- ariadne/logger.py
+++ ariadne/logger.py
@@ -5,14 +5,17 @@ from .utils import unwrap_graphql_error
 
 
 def log_error(
-    error: Exception, logger: Union[None, str, logging.Logger, logging.LoggerAdapter]
+    error: Exception,
+    logger: Union[None, str, logging.Logger, logging.LoggerAdapter],
 ):
     original_error = unwrap_graphql_error(error)
     if original_error and original_error is not error:
         error.__suppress_context__ = True
         error.__cause__ = original_error
 
-    if not logger or isinstance(logger, str):
-        logger = logging.getLogger(logger or "ariadne")
+    if not logger:
+        logger = "ariadne"
+    if isinstance(logger, str):
+        logger = logging.getLogger(logger)
 
     logger.error(error, exc_info=error)

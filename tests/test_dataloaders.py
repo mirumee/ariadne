d730da0d8a01bd93e0de8330fec72eb0f32eb673
yurihs <yurisalvador@hotmail.com>
2022-11-14 14:52:13 -0300

    Add tests for dataloaders

---
+++ tests/test_dataloaders.py
@@ -0,0 +1,63 @@
+from unittest.mock import AsyncMock, Mock
+
+import pytest
+from aiodataloader import DataLoader as AsyncDataLoader
+from graphql_sync_dataloaders import DeferredExecutionContext, SyncDataLoader
+
+from ariadne import QueryType, graphql, graphql_sync, make_executable_schema
+
+
+@pytest.mark.asyncio
+async def test_graphql_supports_async_dataloaders():
+    type_defs = """
+        type Query {
+            test(arg: ID!): String!
+        }
+    """
+
+    dataloader_fn = AsyncMock(side_effect=lambda keys: keys)
+    dataloader = AsyncDataLoader(dataloader_fn)
+
+    query = QueryType()
+    query.set_field("test", lambda *_, arg: dataloader.load(arg))
+
+    schema = make_executable_schema(
+        type_defs,
+        [query],
+    )
+
+    success, result = await graphql(
+        schema,
+        {"query": "{ test1: test(arg: 1), test2: test(arg: 2) }"},
+    )
+    assert success
+    assert result["data"] == {"test1": "1", "test2": "2"}
+    dataloader_fn.assert_called_once_with(["1", "2"])
+
+
+def test_graphql_sync_supports_sync_dataloaders():
+    type_defs = """
+        type Query {
+            test(arg: ID!): String!
+        }
+    """
+
+    dataloader_fn = Mock(side_effect=lambda keys: keys)
+    dataloader = SyncDataLoader(dataloader_fn)
+
+    query = QueryType()
+    query.set_field("test", lambda *_, arg: dataloader.load(arg))
+
+    schema = make_executable_schema(
+        type_defs,
+        [query],
+    )
+
+    success, result = graphql_sync(
+        schema,
+        {"query": "{ test1: test(arg: 1), test2: test(arg: 2) }"},
+        execution_context_class=DeferredExecutionContext,
+    )
+    assert success
+    assert result["data"] == {"test1": "1", "test2": "2"}
+    dataloader_fn.assert_called_once_with(["1", "2"])

5540d8e446b5b1834eae6d996bfd9a4078b6555e
Rafał Pitoń <rafal@mirumee.com>
2022-12-12 16:29:37 +0100

    Add python 3.11 to build matrix

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ CLASSIFIERS = [
     "Programming Language :: Python :: 3.8",
     "Programming Language :: Python :: 3.9",
     "Programming Language :: Python :: 3.10",
+    "Programming Language :: Python :: 3.11",
     "Topic :: Software Development :: Libraries :: Python Modules",
 ]
 

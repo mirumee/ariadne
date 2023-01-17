0e2d5de376cf74164a789ed83dea208af5cebba9
pylipp <beth.aleph@yahoo.de>
2023-01-17 17:08:01 +0100

    Don't accept bytes as path
    we can't assume what kind of encoding the string passed to us as bytes
    is

--- ariadne/load_schema.py
+++ ariadne/load_schema.py
@@ -7,16 +7,14 @@ from graphql.error import GraphQLSyntaxError
 from .exceptions import GraphQLFileSyntaxError
 
 
-def load_schema_from_path(path: Union[str, bytes, os.PathLike]) -> str:
+def load_schema_from_path(path: Union[str, os.PathLike]) -> str:
     if os.path.isdir(path):
         schema_list = [read_graphql_file(f) for f in sorted(walk_graphql_files(path))]
         return "\n".join(schema_list)
     return read_graphql_file(os.path.abspath(path))
 
 
-def walk_graphql_files(
-    path: Union[str, bytes, os.PathLike]
-) -> Generator[str, None, None]:
+def walk_graphql_files(path: Union[str, os.PathLike]) -> Generator[str, None, None]:
     extensions = (".graphql", ".graphqls", ".gql")
     for dirpath, _, files in os.walk(str(path)):
         for name in files:
@@ -24,7 +22,7 @@ def walk_graphql_files(
                 yield os.path.join(dirpath, name)
 
 
-def read_graphql_file(path: Union[str, bytes, os.PathLike]) -> str:
+def read_graphql_file(path: Union[str, os.PathLike]) -> str:
     with open(path, "r", encoding="utf-8") as graphql_file:
         schema = graphql_file.read()
     try:

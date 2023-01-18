0f7167e465b66375229f6ed949a032a811ade25f
P M <10617122+pylipp@users.noreply.github.com>
2023-01-18 12:37:00 +0100

    Make load-schema functions accept path-like objects (#1005)

--- ariadne/load_schema.py
+++ ariadne/load_schema.py
@@ -1,5 +1,5 @@
 import os
-from typing import Generator
+from typing import Generator, Union
 
 from graphql import parse
 from graphql.error import GraphQLSyntaxError
@@ -7,22 +7,22 @@ from graphql.error import GraphQLSyntaxError
 from .exceptions import GraphQLFileSyntaxError
 
 
-def load_schema_from_path(path: str) -> str:
+def load_schema_from_path(path: Union[str, os.PathLike]) -> str:
     if os.path.isdir(path):
         schema_list = [read_graphql_file(f) for f in sorted(walk_graphql_files(path))]
         return "\n".join(schema_list)
     return read_graphql_file(os.path.abspath(path))
 
 
-def walk_graphql_files(path: str) -> Generator[str, None, None]:
+def walk_graphql_files(path: Union[str, os.PathLike]) -> Generator[str, None, None]:
     extensions = (".graphql", ".graphqls", ".gql")
-    for dirpath, _, files in os.walk(path):
+    for dirpath, _, files in os.walk(str(path)):
         for name in files:
             if name.lower().endswith(extensions):
                 yield os.path.join(dirpath, name)
 
 
-def read_graphql_file(path: str) -> str:
+def read_graphql_file(path: Union[str, os.PathLike]) -> str:
     with open(path, "r", encoding="utf-8") as graphql_file:
         schema = graphql_file.read()
     try:

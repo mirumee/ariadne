93dd4f593680e8087169f1fcd84038904cecbe20
Rafał Pitoń <rafal@mirumee.com>
2023-02-08 20:04:36 +0100

    Fix types

--- ariadne/exceptions.py
+++ ariadne/exceptions.py
@@ -1,4 +1,5 @@
-from typing import Optional
+import os
+from typing import Optional, Union
 
 from .constants import HTTP_STATUS_400_BAD_REQUEST
 
@@ -28,30 +29,33 @@ class HttpBadRequestError(HttpError):
 class GraphQLFileSyntaxError(Exception):
     """Raised by `load_schema_from_path` when loaded GraphQL file has invalid syntax."""
 
-    def __init__(self, schema_file: str, message: str) -> None:
+    def __init__(self, file_path: Union[str, os.PathLike], message: str) -> None:
         """Initializes the `GraphQLFileSyntaxError` with file name and error.
         
         # Required arguments
 
-        `schema_file`: a `str` with a name of schema file that failed to validate.
+        `file_path`: a `str` or `PathLike` object pointing to a file that 
+        failed to validate.
 
         `message`: a `str` with validation message.
         """
         super().__init__()
-        self.message = self.format_message(schema_file, message)
 
-    def format_message(self, schema_file: str, message: str):
+        self.message = self.format_message(file_path, message)
+
+    def format_message(self, file_path: Union[str, os.PathLike], message: str):
         """Builds final error message from path to schema file and error message.
         
         Returns `str` with final error message.
         
         # Required arguments
 
-        `schema_file`: a `str` with a name of schema file that failed to validate.
+        `file_path`: a `str` or `PathLike` object pointing to a file that 
+        failed to validate.
 
         `message`: a `str` with validation message.
         """
-        return f"Could not load {schema_file}:\n{message}"
+        return f"Could not load {file_path}:\n{message}"
 
     def __str__(self):
         """Returns error message."""

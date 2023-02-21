bd827e2b2cdc8574862c5e1a01725dcdd11e3991
Rafał Pitoń <rafal@mirumee.com>
2023-02-21 13:54:44 +0100

    Update test message

--- tests/asgi/test_configuration.py
+++ tests/asgi/test_configuration.py
@@ -671,7 +671,7 @@ def test_init_wait_timeout_graphql_transport_ws(
             ws.receive_json()
 
 
-@pytest.mark.xfail(reason="this test sometimes fails due to race condition")
+@pytest.mark.xfail(reason="sometimes fails due to a race condition")
 def test_handle_connection_init_timeout_handler_executed_graphql_transport_ws(
     schema,
 ):

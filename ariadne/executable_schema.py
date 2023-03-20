c43cdaab1da84bf1f52556727369609ecd46d52c
Rafał Pitoń <rafal@mirumee.com>
2023-03-20 13:53:00 +0100

    Tweak normalization error message (#1054)

--- ariadne/executable_schema.py
+++ ariadne/executable_schema.py
@@ -373,7 +373,7 @@ def normalize_bindables(*bindables: SchemaBindables) -> List[SchemaBindable]:
         elif issubclass(bindable, Enum):
             normal_bindables.append(EnumType(bindable.__name__, bindable))
         else:
-            raise ValueError(f"Unknown type: {repr(bindable)}")
+            raise ValueError(f"Unsupported type: {repr(bindable)}")
 
     return normal_bindables
 

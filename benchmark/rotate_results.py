4d351e40bafcbeb59cddaa2c5b29f33024a52eb2
Rafał Pitoń <rafal@mirumee.com>
2023-04-20 16:40:26 +0200

    Run benchmarks periodically and rotate too many results

---
+++ benchmark/rotate_results.py
@@ -0,0 +1,36 @@
+import sys
+from pathlib import Path
+
+from conf import RESULTS_DIR, RESULTS_LIMIT
+
+
+def main():
+    if len(sys.argv) != 2:
+        raise Exception(
+            "'rotate_results.py' should be called with single "
+            "arg: a substr for name with benchmarks dir to rotate"
+        )
+
+    RESULTS_SUBSTR = str(sys.argv[1])
+
+    for sub_results_dir in Path(RESULTS_DIR).glob("**"):
+        if sub_results_dir.is_dir() and RESULTS_SUBSTR in sub_results_dir.name:
+            rotate_results(sub_results_dir)
+
+
+def rotate_results(results_dir: Path):
+    json_files = []
+    for json_file in results_dir.glob("*.json"):
+        if json_file.is_file():
+            json_files.append(json_file.name)
+
+    json_files = sorted(json_files, reverse=True)
+    if len(json_files) > RESULTS_LIMIT:
+        for file_to_delete in json_files[RESULTS_LIMIT:]:
+            print(f"Removed: {results_dir / file_to_delete}")
+            file_path = results_dir / file_to_delete
+            file_path.unlink()
+
+
+if __name__ == "__main__":
+    main()

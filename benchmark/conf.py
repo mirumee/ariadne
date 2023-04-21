3a92cb7f0aa95b2c1148e88ab4c06b612ead40f4
Rafał Pitoń <rafal@mirumee.com>
2023-04-21 12:46:09 +0200

    Run benchmarks periodically and rotate their results (#1075)
    Co-authored-by: Mateusz Sopiński <mateuszsopinski243@gmail.com>

--- benchmark/conf.py
+++ benchmark/conf.py
@@ -2,5 +2,8 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
+RESULTS_LIMIT = 30
+RESULTS_DIR = os.path.join(BASE_DIR, "results")
+
 DATABASE_PATH = os.path.join(BASE_DIR, "database.json")
 SCHEMA_PATH = os.path.join(BASE_DIR, "schema.graphql")

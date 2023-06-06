83c585ccef26c2e03ccfd8aabf904d3c4d56dbab
Rafał Pitoń <rafal@mirumee.com>
2023-06-06 17:03:32 +0200

    Update snapshots

--- tests/tracing/snapshots/snap_test_apollotracing.py
+++ tests/tracing/snapshots/snap_test_apollotracing.py
@@ -35,34 +35,6 @@ snapshots['test_apollotracing_extension_adds_tracing_data_to_async_result_extens
     }
 }
 
-snapshots['test_apollotracing_extension_adds_tracing_data_to_result_extensions 1'] = {
-    'data': {
-        'status': True
-    },
-    'extensions': {
-        'tracing': {
-            'duration': 0,
-            'endTime': '2012-01-14T03:21:34.000000Z',
-            'execution': {
-                'resolvers': [
-                    {
-                        'duration': 0,
-                        'fieldName': 'status',
-                        'parentType': 'Query',
-                        'path': [
-                            'status'
-                        ],
-                        'returnType': 'Boolean',
-                        'startOffset': 0
-                    }
-                ]
-            },
-            'startTime': '2012-01-14T03:21:34.000000Z',
-            'version': 1
-        }
-    }
-}
-
 snapshots['test_apollotracing_extension_adds_tracing_data_to_sync_result_extensions 1'] = {
     'data': {
         'status': True
@@ -89,10 +61,6 @@ snapshots['test_apollotracing_extension_adds_tracing_data_to_sync_result_extensi
             'version': 1
         }
     }
-}
-
-snapshots['test_apollotracing_extension_handles_exceptions_in_resolvers 1'] = {
-    'testError': None
 }
 
 snapshots['test_apollotracing_extension_handles_exceptions_in_resolvers_in_async_context 1'] = {

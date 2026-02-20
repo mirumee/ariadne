f8f0d3bdd9044205bd0807955730509bdb4f89d7
Karol Jagodziński <karoljago@gmail.com>
2026-02-20 11:42:21 +0100

    refactor!: remove deprecated apollo tracing, opentracing, and extend_federated_schema

--- benchmark/test_extensions.py
+++ benchmark/test_extensions.py
@@ -5,7 +5,6 @@ from starlette.testclient import TestClient
 from ariadne.asgi import GraphQL
 from ariadne.asgi.handlers import GraphQLHTTPHandler
 from ariadne.contrib.tracing.opentelemetry import OpenTelemetryExtension
-from ariadne.contrib.tracing.opentracing import OpenTracingExtension
 from ariadne.types import Extension
 
 from .schema import schema
@@ -55,28 +54,6 @@ def test_query_with_open_telemetry_extension(benchmark, benchmark_query):
     app = GraphQL(
         schema,
         http_handler=GraphQLHTTPHandler(extensions=[OpenTelemetryExtension]),
-    )
-
-    client = TestClient(app)
-
-    def api_call():
-        return client.post(
-            "/",
-            json={
-                "operationName": "GetThreads",
-                "query": benchmark_query,
-            },
-        )
-
-    result = benchmark(api_call)
-    assert result.status_code == HTTPStatus.OK
-    assert not result.json().get("errors")
-
-
-def test_query_with_open_tracing_extension(benchmark, benchmark_query):
-    app = GraphQL(
-        schema,
-        http_handler=GraphQLHTTPHandler(extensions=[OpenTracingExtension]),
     )
 
     client = TestClient(app)
